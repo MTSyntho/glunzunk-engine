2ee3862f8efacce133a9ed2723f0ce43d850436c
MTSyntho <phenyosethole0907@gmail.com>
2024-06-15 22:08:36 +0200

    i cant remember tbh, but i did add an editor apparently

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from panda3d.core import loadPrcFile
+import panda3d.core as p3d
 loadPrcFile('config.prc')
 
 from direct.showbase.ShowBase import ShowBase
@@ -88,6 +89,9 @@ class GZEngine(ShowBase):
 	def __init__(self):
 		ShowBase.__init__(self)
 
+#		based = p3d.base()
+		basedfr = aspect2d.attach_new_node("bru")
+
 		# Font Loading
 		lmLight = self.loader.loadFont('assets\\fonts\\egg\\LEMONMILK-Light.egg')
 		lmRegular = self.loader.loadFont('assets\\fonts\\egg\\LEMONMILK-Regular.egg')
@@ -115,6 +119,7 @@ class GZEngine(ShowBase):
 				startLoad()
 				homeMenu()
 
+			base.setBackgroundColor(0.80, 0.80, 0.80)
 			print('NOTE: Displaying Splash Screen [splashScreen()]')
 			splashLogo = OnscreenImage(image='assets\\ui\\gztitle.png', pos=(0, 0, 0), scale=(0.8, 0.1, 0.17))
 			splashLogo.setTransparency(TransparencyAttrib.MAlpha)
@@ -202,27 +207,29 @@ class GZEngine(ShowBase):
 
 		def openProject():
 			def openProjectConfirm():
-				# Get the default display region
-				dr = base.camNode.getDisplayRegion(0)
-				
-				# Clear the screen
-				dr.clear()
-
+				# Clear Screen
+				# Clear the color and depth buffers.
+#				based.render.clear_color()
+#				based.render.clear_depth()
+				basedfr.removeNode()
+				darkBG.destroy()
+				from editor import loadEditor, endEditor
+				loadEditor('__none__')
 
 			print('NOTE: Displaying Open Project Menu[openProject()]')
 			# createProjectFr actually makes the file
-			fadeBlackBG = OnscreenImage(image='assets\\placeholder\\fadeBlack.png', pos=(0, 0, 0), scale=100)
+			fadeBlackBG = OnscreenImage(image='assets\\placeholder\\fadeBlack.png', pos=(0, 0, 0), scale=100, parent=basedfr)
 			fadeBlackBG.setTransparency(TransparencyAttrib.MAlpha)
-			darkCreateBG = OnscreenImage(image='assets\\placeholder\\lightDark.png', pos=(0, 0, 0), scale=(1.1, 0, 0.4))
-			opentitle = OnscreenText(text='OPEN AN EXISTING PROJECT', pos=(-0.45, 0.17), scale=0.15, fg=(1, 1, 1, 1))
+			darkCreateBG = OnscreenImage(image='assets\\placeholder\\lightDark.png', pos=(0, 0, 0), scale=(1.1, 0, 0.4), parent=basedfr)
+			opentitle = OnscreenText(text='OPEN AN EXISTING PROJECT', pos=(-0.45, 0.17), scale=0.15, fg=(1, 1, 1, 1), parent=basedfr)
 			opentitle.setFont(bbn)
-			projectnametitle = OnscreenText(text='PROJECT NAME:', pos=(-0.70, 0.05), scale=0.11, fg=(1, 1, 1, 1))
+			projectnametitle = OnscreenText(text='PROJECT NAME:', pos=(-0.70, 0.05), scale=0.11, fg=(1, 1, 1, 1), parent=basedfr)
 			projectnametitle.setFont(bbn)
-			projectNameEntry = DirectEntry(text = "", scale=.07,  numLines=1, focus=1, command=openProjectConfirm, width=30, pos=-0.07)
+			projectNameEntry = DirectEntry(text = "", scale=.07,  numLines=1, focus=1, command=openProjectConfirm, width=30, pos=-0.07, parent=basedfr)
 			projectNameEntry.set_x(-1.05)
 #			print(projectNameEntry.textEntered())
-			openProjectBtn = DirectButton(relief=None, image='assets\\placeholder\\dark.png', pos=(-0.85, 0, -0.24), scale=(0.15, 1, 0.06), command=openProjectConfirm)
-			openProjectBtnTxt = OnscreenText(text='Open', pos=(-0.85, -0.27), scale=0.10, fg=(1, 1, 1, 1))
+			openProjectBtn = DirectButton(relief=None, image='assets\\placeholder\\dark.png', pos=(-0.85, 0, -0.24), scale=(0.15, 1, 0.06), command=openProjectConfirm, parent=basedfr)
+			openProjectBtnTxt = OnscreenText(text='Open', pos=(-0.85, -0.27), scale=0.10, fg=(1, 1, 1, 1), parent=basedfr)
 			openProjectBtnTxt.setFont(cvc)
 
 
@@ -231,21 +238,21 @@ class GZEngine(ShowBase):
 				print('Oh boy oh boy, i am in for a real treat :|')
 
 			print('NOTE: Displaying Home Menu [homeMenu()]')
-			title = OnscreenText(text='Glunzunk Engine', pos=(-1.0, 0.70), scale=0.12, fg=(1, 1, 1, 1)) #, parent=base.aspect2d(a2dTopLeft))
+			title = OnscreenText(text='Glunzunk Engine', pos=(-1.0, 0.70), scale=0.12, fg=(1, 1, 1, 1), parent=basedfr) #, parent=base.aspect2d(a2dTopLeft))
 			lmRegular = self.loader.loadFont('assets\\fonts\\egg\\LEMONMILK-Regular.egg')
 			title.setFont(lmRegular)
-			p3dText = OnscreenText(text='Powered by Panda3D', pos=(-1.28, 0.62), scale=0.05, fg=(1, 1, 1, 1))
+			p3dText = OnscreenText(text='Powered by Panda3D', pos=(-1.28, 0.62), scale=0.05, fg=(1, 1, 1, 1), parent=basedfr)
 			lmLight = self.loader.loadFont('assets\\fonts\\egg\\LEMONMILK-Light.egg')
 			p3dText.setFont(lmLight)
-			versionText = OnscreenText(text=version, pos=(-1.42, 0.56), scale=0.04, fg=(1, 1, 1, 1))
+			versionText = OnscreenText(text=version, pos=(-1.42, 0.56), scale=0.04, fg=(1, 1, 1, 1), parent=basedfr)
 #			lmLight = self.loader.loadFont('assets\\fonts\\egg\\LEMONMILK-Light.egg')
 			versionText.setFont(lmLight)
 
 			# Left Side Buttons
 			# Create Project Button
 #			createBox = OnscreenImage(image='assets\\placeholder\\lightdark.png', pos=(-1, 0, 0.3), scale=(0.5, 0, 0.12))
-			createBtn = DirectButton(relief=None, image='assets\\placeholder\\lightdark.png', pos=(-1, 0, 0.3), scale=(0.5, 1, 0.12), command=createProject)
-			createText = OnscreenText(text='CREATE PROJECT', pos=(-1, 0.24), scale=0.18, fg=(1, 1, 1, 1))
+			createBtn = DirectButton(relief=None, image='assets\\placeholder\\lightdark.png', pos=(-1, 0, 0.3), scale=(0.5, 1, 0.12), command=createProject, parent=basedfr)
+			createText = OnscreenText(text='CREATE PROJECT', pos=(-1, 0.24), scale=0.18, fg=(1, 1, 1, 1), parent=basedfr)
 #			cvc = self.loader.loadFont('assets\\fonts\\egg\\coolvetica condensed rg.egg')
 			createText.setFont(cvc)
 #			clickCreateText = DirectButton(text=' ', pos=(-1, 0, 0.25), scale=(1.5, 0, 0.15), text_bg=(1, 1, 1, 1), command=createProject)
@@ -254,8 +261,8 @@ class GZEngine(ShowBase):
 
 			# Project Properties Button
 #			propBox = OnscreenImage(image='assets\\placeholder\\lightdark.png', pos=(-1, 0, 0), scale=(0.5, 0, 0.12))
-			propBtn = DirectButton(relief=None, image='assets\\placeholder\\lightdark.png', pos=(-1, 0, 0), scale=(0.5, 1, 0.12))
-			propText = OnscreenText(text='PROJECT INFO', pos=(-1, -0.06), scale=0.18, fg=(1, 1, 1, 1))
+			propBtn = DirectButton(relief=None, image='assets\\placeholder\\lightdark.png', pos=(-1, 0, 0), scale=(0.5, 1, 0.12), parent=basedfr)
+			propText = OnscreenText(text='PROJECT INFO', pos=(-1, -0.06), scale=0.18, fg=(1, 1, 1, 1), parent=basedfr)
 #			cvc = self.loader.loadFont('assets\\fonts\\egg\\coolvetica condensed rg.egg')
 			propText.setFont(cvc)
 #			clickPropText = DirectButton(text=' ', pos=(-1, 0, -0.05), scale=(1.5, 0, 0.15), text_bg=(1, 1, 1, 1), command=createProject)
@@ -263,14 +270,14 @@ class GZEngine(ShowBase):
 
 			# Settings Button
 #			settingsBox = OnscreenImage(image='assets\\placeholder\\lightdark.png', pos=(-1, 0, -0.3), scale=(0.5, 0, 0.12))
-			settingsBtn = DirectButton(relief=None, image='assets\\placeholder\\lightdark.png', pos=(-1, 0, -0.3), scale=(0.5, 1, 0.12))
-			settingsText = OnscreenText(text='SETTINGS', pos=(-1, -0.36), scale=0.18, fg=(1, 1, 1, 1))
+			settingsBtn = DirectButton(relief=None, image='assets\\placeholder\\lightdark.png', pos=(-1, 0, -0.3), scale=(0.5, 1, 0.12), parent=basedfr)
+			settingsText = OnscreenText(text='SETTINGS', pos=(-1, -0.36), scale=0.18, fg=(1, 1, 1, 1), parent=basedfr)
 #			cvc = self.loader.loadFont('assets\\fonts\\egg\\coolvetica condensed rg.egg')
 			settingsText.setFont(cvc)
 #			clickSettingsText = DirectButton(text=' ', pos=(-1, 0, -0.35), scale=(1.5, 0, 0.15), text_bg=(1, 1, 1, 1), relief=None, command=createProject, image=('assets\\textures\\tiled_Green Background.png'))
 #			clickSettingsText.hide()
 
-			projectList = DirectScrolledFrame(canvasSize=(-.80, .70, -2, 8), frameSize=(-.80, .80, -1.2, .40), scrollBarWidth=0.05)
+			projectList = DirectScrolledFrame(canvasSize=(-.80, .70, -2, 8), frameSize=(-.80, .80, -1.2, .40), scrollBarWidth=0.05, parent=basedfr)
 			projectList.setPos(0.80, 0, 0.38)
 			projectList.setColor(.22, .22, .22)
 
@@ -281,7 +288,8 @@ class GZEngine(ShowBase):
 			endLoad()
 
 #		createProject()
-		splashScreen()
+#		splashScreen()
+		openProject()
 #		homeMenu()
 
 gz = GZEngine()
@@ -290,3 +298,4 @@ gz.run()
 # This is MTSyntho at 9:54 PM on 2023/09/24, i'm suffering tryna get project creation working and the project entry positioned correctly
 # This is MTSyntho at 8:41 AM on 2023/09/25, i love google bard now, not in that sus way though :skull: :moyai:
 # im such a fukin dumbass rn at 10:32 PM on 2023/09/28, not even bard can help
+# Thanks rdb from helping me with menu deletion at 11:55 PM on 2023/09/29
