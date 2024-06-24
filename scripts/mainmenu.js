bf6755b30785dc8b9e6056186a268ed2d7932054
MTSyntho <phenyosethole0907@gmail.com>
2024-06-24 03:08:58 +0200

    added project dialog in main menu
    added project creation dialog
    also added file picker

---
+++ scripts/mainmenu.js
@@ -0,0 +1,41 @@
+function projectdialog(status) {
+    var overlay = document.getElementById("overlay");
+    var main = document.getElementById("main");
+    var name = document.getElementById("name");
+
+    if (status == "show") {
+        overlay.classList.add("fadeIn");
+        overlay.classList.remove("fadeOut");
+        name.value="";
+    }
+
+    if (status == "hide") {
+        overlay.classList.remove("fadeIn");
+        overlay.classList.add("fadeOut");
+    }
+}
+
+async function createproject() {
+  try {
+    const dirHandle = await window.showDirectoryPicker();
+    
+    // You can now work with the directory handle (dirHandle)
+    console.log("Selected directory:", dirHandle.name);
+    
+    // Example: List all files in the selected directory
+    const iterator = await dirHandle.values();
+    for await (const entry of iterator) {
+      console.log("File:", entry.name);
+    }
+    var main = document.getElementById("main");
+    var overlay = document.getElementById("overlay");
+    main.classList.remove('fadeIn')
+    main.classList.add('fadeOut')
+    overlay.classList.remove('fadeIn')
+    overlay.classList.add('fadeOut')
+  } catch (err) {
+    console.error("Error selecting directory:", err);
+  }
+}
+
+// thx chatgtp
