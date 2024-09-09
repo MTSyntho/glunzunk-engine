7ab1ca8611e1c80998dfc7d45606b2c8099df3eb
MTSyntho <phenyosethole0907@gmail.com>
2024-09-09 21:42:44 +0200

    added babylon, and it works
    wow

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
 import webview
 
-webview.create_window('Glunzunk Engine', url='index.html', background_color='#2C2C2C', width=1280, height=720)
-webview.start(private_mode=False)
+webview.create_window('Glunzunk Engine', url='index.html', background_color='#000000', width=1280, height=720)
+webview.start(private_mode=False, debug=True)
