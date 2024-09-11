f6845c2572078b63b15058d96791846a82065e97
MTSyntho <phenyosethole0907@gmail.com>
2024-09-11 22:22:45 +0200

    Added window to create objects, doesnt workyet

--- editor.js
+++ editor.js
@@ -65,8 +65,7 @@ function ObjectsPanel(state) {
       top: '50px',
       min: state,
       class: [ "no-full", "no-close" ],
-      innerHTML: `
-      <p>might replace with embed instead of innerhtml</p>`,
+      url: 'embed-panels/objectsPanel/index.html',
       overflow: true,
       onclose: function(){
         this.g.classList.add("windowClose");
@@ -87,6 +86,37 @@ function ObjectsPanel(state) {
 SceneExplorer(true);
 InspectorPanel(true);
 ObjectsPanel(true);
+
+function windowCreateObject() {
+  var winbox = new WinBox({
+      title: 'Choose an Object',
+      max: false,
+      top: '50px',
+      class: [ "no-full" ],
+      url: '',
+      overflow: true,
+      onclose: function(){
+        this.g.classList.add("windowClose");
+        document.getElementById(winbox.id).classList.remove("opentask");
+        document.getElementById(winbox.id).classList.add("closetask");
+
+        setTimeout(() => {
+          this.onclose = null;
+          this.close();
+          document.getElementById(winbox.id).remove();
+        }, 200);
+
+        return true;
+      }
+  });
+};
+
+// Listen for messages from iframe (taken from zdkrimson)
+window.addEventListener('message', function(event) {
+    if (event.data == 'windowCreateObject') {
+        windowCreateObject();
+    };
+});
 
 // Get the canvas DOM element
 var canvas = document.getElementById('renderCanvas');
---
+++ embed-panels/objectsPanel/index.js
@@ -0,0 +1,7 @@
+/*
+    Objects Panel - Glunzunk Engine
+    Written by: MTSyntho - Sep 2024
+*/
+
+// Send message from iframe to parent window (taken from zdkrimson)
+// window.parent.postMessage('windowCreateObject', '*');
